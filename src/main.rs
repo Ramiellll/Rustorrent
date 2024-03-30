a9929a823ae3d4c06e59cd65677db6b377bf2e55
Ramiellll <ramyabdulkadir40@gmail.com>
2024-03-30 22:27:14 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+fn main() -> eframe::Result<()> {
+    env_logger::init();
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Rustorrent",
+        native_options,
+        Box::new(|cc| Box::new(rustorrent::TemplateApp::new(cc))),
+    )
+}
